b599c5daa3402c8426ca68a6ffe7fdda741a1bce
Hady Rashwan <h2rashwan@gmail.com>
2024-09-11 19:42:27 +0100

    fix(app): move to green style

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,37 @@
-import { useState, useEffect } from "react";
-import {
-  ChakraProvider,
-  Box,
-  Heading,
-  Input,
-  Button,
-  Text,
-  Link,
-  Image,
-  VStack,
-  HStack,
-  useClipboard,
-  Divider,
-} from "@chakra-ui/react";
-import duckduckgoLogo from "./assets/duckduckgo_logo.svg";
-import "./App.css";
+import { useState, useEffect } from 'react';
+import { ChakraProvider, Box, Heading, Input, Button, Text, Link, Image, VStack, HStack, useClipboard, Divider, Icon, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { CopyIcon, CheckIcon } from '@chakra-ui/icons';
+import duckduckgoLogo from './assets/DuckAlias_sender_512x512-removebg-preview-transformed.webp';
+import './App.css';
 
 function App() {
-  const [input1, setInput1] = useState("");
-  const [input2, setInput2] = useState("");
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
   const [showGeneratedEmail, setShowGeneratedEmail] = useState(false);
-  const [generatedEmail, setGeneratedEmail] = useState("");
+  const [generatedEmail, setGeneratedEmail] = useState('');
   const { hasCopied, onCopy } = useClipboard(generatedEmail);
+  const { colorMode } = useColorMode();
 
   // Retrieve from localStorage when the app loads
   useEffect(() => {
-    const duckEmailInput = localStorage.getItem("duck_email");
-    const recipientEmailInput = localStorage.getItem("recipient_email");
-    if (duckEmailInput) setInput1(duckEmailInput);
-    if (recipientEmailInput) setInput2(recipientEmailInput);
+    const storedInput1 = localStorage.getItem('duck_input1');
+    const storedInput2 = localStorage.getItem('duck_input2');
+    if (storedInput1) setInput1(storedInput1);
+    if (storedInput2) setInput2(storedInput2);
   }, []);
 
   // Update localStorage when inputs change
   useEffect(() => {
-    localStorage.setItem("duck_email", input1);
+    localStorage.setItem('duck_input1', input1);
   }, [input1]);
 
   useEffect(() => {
-    localStorage.setItem("recipient_email", input2);
+    localStorage.setItem('duck_input2', input2);
   }, [input2]);
 
   const handleClick = () => {
-    if (input1.includes("@") && input2.includes("@")) {
-      const alias = `${input2.replace("@", "_at_")}_${input1}`;
+    if (input1.includes('@') && input2.includes('@')) {
+      const alias = `${input2.replace('@', '_at_')}_${input1}`;
       setGeneratedEmail(alias);
       setShowGeneratedEmail(true);
     } else {
@@ -50,17 +39,31 @@ function App() {
     }
   };
 
+  // Define green color scheme
+  const greenColorScheme = {
+    light: {
+      textColor: 'green.800',
+      boxBgColor: 'green.50',
+      buttonColorScheme: 'green',
+      dividerColor: 'green.200',
+    },
+    dark: {
+      textColor: 'white.200',
+      boxBgColor: 'green.900',
+      buttonColorScheme: 'green',
+      dividerColor: 'green.700',
+    },
+  };
+
+  // Get color scheme based on the current color mode
+  const colors = useColorModeValue(greenColorScheme.light, greenColorScheme.dark);
+
   return (
     <ChakraProvider>
       <VStack spacing={8} align="center" className="card" padding="4">
         {/* Logo and Header */}
         <Box as="a" href="https://vitejs.dev" target="_blank">
-          <Image
-            src={duckduckgoLogo}
-            alt="DuckDuckGo logo"
-            className="logo"
-            boxSize="120px"
-          />
+          <Image src={duckduckgoLogo} alt="DuckDuckGo logo" className="logo" boxSize="260px" />
         </Box>
 
         <Heading size="lg" textAlign="center">
@@ -69,14 +72,14 @@ function App() {
 
         {/* Introduction */}
         <Box textAlign="center" maxW="450px">
-          <Text fontSize="md" color="gray.600">
+          <Text fontSize="md" textAlign="center">
             Use your pre-generated DuckDuckGo aliases to send emails. Enter your
             DuckDuckGo email and the recipient's email below to generate an
             alias.
           </Text>
         </Box>
 
-        <Divider borderColor="gray.300" />
+        <Divider borderColor={colors.dividerColor} />
 
         {/* Input Fields */}
         <VStack spacing={4} width="100%" maxW="400px">
@@ -85,7 +88,7 @@ function App() {
             value={input1}
             onChange={(e) => setInput1(e.target.value)}
             placeholder="Your @duck.com email"
-            focusBorderColor="orange.400"
+            focusBorderColor="green.400"
             size="lg"
           />
 
@@ -94,12 +97,12 @@ function App() {
             value={input2}
             onChange={(e) => setInput2(e.target.value)}
             placeholder="Recipient's email"
-            focusBorderColor="orange.400"
+            focusBorderColor="green.400"
             size="lg"
           />
 
           <Button
-            colorScheme="orange"
+            colorScheme="green"
             size="lg"
             onClick={handleClick}
             isFullWidth
@@ -110,48 +113,28 @@ function App() {
 
         {/* Generated Alias */}
         {showGeneratedEmail && (
-          <Box
-            textAlign="center"
-            p={4}
-            borderWidth={1}
-            borderRadius="lg"
-            shadow="md"
-            width="100%"
-            maxW="400px"
-          >
-            <Text fontSize="lg" color="gray.800" mb={2}>
+          <Box textAlign="center" p={4} borderWidth={1} borderRadius="lg" shadow="md" width="100%" maxW="400px" bg={colors.boxBgColor}>
+            <Text fontSize="lg" color={colors.textColor} mb={2}>
               Send the email to this alias:
             </Text>
-            <HStack justify="center">
-              <Text fontWeight="bold" color="blue.500">
+            <HStack justify="center" spacing={4}>
+              <Text fontWeight="bold" color="green.500" fontSize="lg">
                 {generatedEmail}
               </Text>
-              <Button size="sm" colorScheme="blue" onClick={onCopy}>
-                {hasCopied ? "Copied" : "Copy"}
+              <Button size="sm" colorScheme={colors.buttonColorScheme} onClick={onCopy}>
+                <Icon as={hasCopied ? CheckIcon : CopyIcon} />
               </Button>
             </HStack>
           </Box>
         )}
 
         {/* Footer */}
-        <Box
-          p={4}
-          borderWidth={1}
-          borderRadius="md"
-          textAlign="center"
-          width="100%"
-          maxW="400px"
-          bg="gray.50"
-        >
-          <Text fontSize="sm" color="gray.600">
-            This is not an official tool by DuckDuckGo. It's{" "}
-            <Link
-              href="https://github.com/hadyrashwan/duckduckgo-sender-alias"
-              isExternal
-              color="orange.500"
-            >
+        <Box p={4} borderWidth={1} borderRadius="md" textAlign="center" width="100%" maxW="400px" bg={colors.boxBgColor}>
+          <Text fontSize="sm" color={colors.textColor}>
+            This is not an official tool by DuckDuckGo. It's{' '}
+            <Link href="https://github.com/hadyrashwan/duckduckgo-sender-alias" isExternal color="green.500">
               open-source
-            </Link>{" "}
+            </Link>{' '}
             and doesn't collect your data.
           </Text>
         </Box>
