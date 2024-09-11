723d09f22ed9859aae9110b151efbd9e93d9ee71
Hady Rashwan <h2rashwan@gmail.com>
2024-09-11 20:02:30 +0100

    fix(app): css updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { ChakraProvider, Box, Heading, Input, Button, Text, Link, Image, VStack, HStack, useClipboard, Divider, Icon, useColorMode, useColorModeValue } from '@chakra-ui/react';
+import { ChakraProvider, Box, Heading, Input, Button, Text, Link, Image, VStack, HStack, Divider, Icon, useClipboard, useColorMode, useColorModeValue } from '@chakra-ui/react';
 import { CopyIcon, CheckIcon } from '@chakra-ui/icons';
 import duckduckgoLogo from './assets/DuckAlias_sender_512x512-removebg-preview-transformed.webp';
 import './App.css';
@@ -58,87 +58,97 @@ function App() {
   // Get color scheme based on the current color mode
   const colors = useColorModeValue(greenColorScheme.light, greenColorScheme.dark);
 
+  // Set CSS variables for colors
+  useEffect(() => {
+    document.documentElement.style.setProperty('--text-color', colors.textColor);
+    document.documentElement.style.setProperty('--box-bg-color', colors.boxBgColor);
+    document.documentElement.style.setProperty('--button-bg-color', colors.buttonColorScheme);
+    document.documentElement.style.setProperty('--button-text-color', colors.textColor);
+  }, [colors]);
+
   return (
     <ChakraProvider>
-      <VStack spacing={8} align="center" className="card" padding="4">
-        {/* Logo and Header */}
-        <Box as="a" href="https://vitejs.dev" target="_blank">
-          <Image src={duckduckgoLogo} alt="DuckDuckGo logo" className="logo" boxSize="260px" />
-        </Box>
-
-        <Heading size="lg" textAlign="center">
-          DuckAlias Sender
-        </Heading>
-
-        {/* Introduction */}
-        <Box textAlign="center" maxW="450px">
-          <Text fontSize="md" color={colors.textColor} textAlign="center">
-            Use your pre-generated DuckDuckGo aliases to send emails. Enter your
-            DuckDuckGo email and the recipient's email below to generate an
-            alias.
-          </Text>
-        </Box>
-
-        <Divider borderColor={colors.dividerColor} />
-
-        {/* Input Fields */}
-        <VStack spacing={4} width="100%" maxW="400px">
-          <Input
-            type="text"
-            value={input1}
-            onChange={(e) => setInput1(e.target.value)}
-            placeholder="Your @duck.com email"
-            focusBorderColor="green.400"
-            size="lg"
-          />
-
-          <Input
-            type="text"
-            value={input2}
-            onChange={(e) => setInput2(e.target.value)}
-            placeholder="Recipient's email"
-            focusBorderColor="green.400"
-            size="lg"
-          />
-
-          <Button
-            colorScheme="green"
-            size="lg"
-            onClick={handleClick}
-            isFullWidth
-          >
-            Generate Alias
-          </Button>
-        </VStack>
-
-        {/* Generated Alias */}
-        {showGeneratedEmail && (
-          <Box textAlign="center" p={4} borderWidth={1} borderRadius="lg" shadow="md" width="100%" maxW="400px" bg={colors.boxBgColor} className="email-box">
-            <Text fontSize="lg" color={colors.textColor} mb={2}>
-              Send the email to this alias:
-            </Text>
-            <HStack justify="center" spacing={4} className="email-container">
-              <Text fontWeight="bold" color="green.500" fontSize="lg" className="email-text">
-                {generatedEmail}
-              </Text>
-              <Button size="sm" colorScheme={colors.buttonColorScheme} onClick={onCopy}>
-                <Icon as={hasCopied ? CheckIcon : CopyIcon} />
-              </Button>
-            </HStack>
+      <Box className="fullscreen-container" display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
+        <VStack spacing={8} align="center" className="card" padding="4" maxWidth="600px" width="100%">
+          {/* Logo and Header */}
+          <Box as="a" href="https://github.com/hadyrashwan/duckduckgo-sender-alias" target="_blank">
+            <Image src={duckduckgoLogo} alt="DuckAlias logo" className="logo" />
           </Box>
-        )}
 
-        {/* Footer */}
-        <Box p={4} borderWidth={1} borderRadius="md" textAlign="center" width="100%" maxW="400px" bg={colors.boxBgColor}>
-          <Text fontSize="sm" color={colors.textColor}>
-            This is not an official tool by DuckDuckGo. It's{' '}
-            <Link href="https://github.com/hadyrashwan/duckduckgo-sender-alias" isExternal color="green.500">
-              open-source
-            </Link>{' '}
-            and doesn't collect your data.
-          </Text>
-        </Box>
-      </VStack>
+          <Heading size="lg" textAlign="center">
+            DuckAlias Sender
+          </Heading>
+
+          {/* Introduction */}
+          <Box textAlign="center" maxW="450px">
+            <Text>
+              Use your pre-generated DuckDuckGo aliases to send emails. Enter your
+              DuckDuckGo email and the recipient's email below to generate an
+              alias.
+            </Text>
+          </Box>
+
+          <Divider className="divider" />
+
+          {/* Input Fields */}
+          <VStack spacing={4} width="100%" maxW="400px">
+            <Input
+              type="text"
+              value={input1}
+              onChange={(e) => setInput1(e.target.value)}
+              placeholder="Your @duck.com email"
+              focusBorderColor="green.400"
+              size="lg"
+            />
+
+            <Input
+              type="text"
+              value={input2}
+              onChange={(e) => setInput2(e.target.value)}
+              placeholder="Recipient's email"
+              focusBorderColor="green.400"
+              size="lg"
+            />
+
+            <Button
+              colorScheme="green"
+              size="lg"
+              onClick={handleClick}
+              isFullWidth
+            >
+              Generate Alias
+            </Button>
+          </VStack>
+
+          {/* Generated Alias */}
+          {showGeneratedEmail && (
+            <Box className="email-box">
+              <Text className="email-text">
+                Send the email to this alias:
+              </Text>
+              <HStack justify="center" spacing={4}>
+                <Text className="email-value">
+                  {generatedEmail}
+                </Text>
+                <Button className="copy-button" onClick={onCopy}>
+                  <Icon as={hasCopied ? CheckIcon : CopyIcon} />
+                </Button>
+              </HStack>
+            </Box>
+          )}
+
+          {/* Footer */}
+          <Box className="footer">
+            <Text className="footer-text">
+              This is not an official tool by DuckDuckGo. It's{' '}
+              <Link href="https://github.com/hadyrashwan/duckduckgo-sender-alias" isExternal color="green.500">
+                open-source
+              </Link>{' '}
+              and doesn't collect your data.
+            </Text>
+          </Box>
+        </VStack>
+      </Box>
     </ChakraProvider>
   );
 }
